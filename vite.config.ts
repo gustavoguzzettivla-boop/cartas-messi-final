d2dbd6d937450ede031fa51721636feb9b7b0265
Gustavo <gustavoguzzettivla@gmail.com>
2026-06-25 18:46:29 -0300

    fix cloudflare vite plugin import

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import cloudflare from "@cloudflare/vite-plugin";
+import * as cloudflare from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
-    cloudflare(),
+    cloudflare.default(),
     TanStackRouterVite(),
     react(),
   ],
