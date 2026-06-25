0bd3ef840a9fcf0633389d47c9c9eb9dafaed4dc
Gustavo <gustavoguzzettivla@gmail.com>
2026-06-25 18:42:07 -0300

    remove tanstack start to fix SSR issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import cloudflare from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
+    cloudflare(),
     TanStackRouterVite(),
     react(),
   ],
