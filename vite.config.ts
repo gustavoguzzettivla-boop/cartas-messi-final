15d2e646c1de5591c04ac38f5650a50e58e5fbe8
Gustavo <gustavoguzzettivla@gmail.com>
2026-06-25 18:49:53 -0300

    remove cloudflare vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import * as cloudflare from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
-    cloudflare.default(),
     TanStackRouterVite(),
     react(),
   ],
